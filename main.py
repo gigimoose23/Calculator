3e70d0260e06023ea26576dd7003e98e2af614d7
Sami Farahat <gigimoosegaming@gmail.com>
2025-02-15 17:36:59 +0000

    k

--- main.py
+++ main.py
@@ -1,50 +1,70 @@
 import tkinter
+import tkinter.messagebox
 root = tkinter.Tk()
 
 root.title("Calculator")
 root.resizable(False, False)
-userEntryVar = tkinter.IntVar()
-userEntryVar.set(0)
+userEntryVar = tkinter.StringVar()
+userEntryVar.set("")
 userEntry = tkinter.Entry(root)
 userEntry.config(textvariable=userEntryVar, font="Arial 15", state="readonly")
 
-userEntry.place(x=0, y=0, width=220, height=48)
-root.geometry("220x200")
+userEntry.place(x=0, y=0, width=253, height=48)
+root.geometry("253x200")
 row = 5
 column = 0
 
+def me():
+    try:
+        userEntryVar.set(eval(doProc(userEntryVar.get())))
+    except Exception as e:
+        tkinter.messagebox.showerror("Error", e)
+
+def doProc(s):
+    x = s.replace(" ", "")
+    y = x.replace("÷", "/")
+    z = y.replace("x", "*")
+    return z
+
 buttons = [ {"Name": "x", "Callback":
 #multiply
- lambda: print("Pressed multiply")
+ lambda: addNum(" x ")
  },{"Name": "+", "Callback":
  
  #add
- lambda: print("Pressed add")
+ lambda: addNum(" + ")
  },{"Name": "÷", "Callback": 
 
 #divide
- lambda: print("Pressed divide")
+ lambda: addNum(" ÷ ")
  },{"Name": "-", "Callback":
 
+    #subtract
+ lambda: addNum(" - ")
+ }, {"Name": "=", "Callback":
 
- lambda: print("Pressed subtract")
+    #eval
+ lambda: me(),
+ }, {"Name": "C", "Callback":
+     
+ lambda: userEntryVar.set(""),
+ 
  }]
 isFirst = True
-maxOnRow = 2
+maxOnRow = 3
 onRow = 0
+#symbols
 for i, button in enumerate(buttons):
     btn = tkinter.Button(root)
-    root.grid_rowconfigure(0, minsize=50)
+    root.grid_rowconfigure(0, minsize=50) 
     btn.config(text=button["Name"],width=2,height=1, font="Arial 14 bold", command=button["Callback"])
-    if isFirst:
-        btn.grid(column=column, row=row )
-    else:
-       
-        btn.grid(column=column, row=row )
+    btn.grid(column=column, row=row )
+        
     onRow +=1
     if onRow == maxOnRow:
+        onRow = 0
+        column = 0
         row+=1
-        column -=1
     else:
         column+=1
     
@@ -57,15 +77,15 @@ maxOnRowN = 3
 onRowN = 0
 rowN = 5
 columnN = 7
-def hi(e):
-    print(e)
+def addNum(e):
+    userEntryVar.set(str(userEntryVar.get()) + str(e))
 for i in range(10):
     btn = tkinter.Button(root)
     root.grid_rowconfigure(0, minsize=50)
     root.grid_columnconfigure(3, minsize=50)
     m = i
     btn.config(text=m,width=2,height=1, font="Arial 14 bold")
-    btn.config( command=lambda: hi(btn.configure()["text"]) )
+    btn.config( command=lambda m=m: addNum(m))
     
     btn.grid(column=columnN, row=rowN )
        
